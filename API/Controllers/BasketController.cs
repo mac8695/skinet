907fbb4e95baf933cececf315c6a8a7b757bb21d
Mahesh Chavan <maheshchavan0806@gmail.com>
2021-07-07 14:49:22 -0400

    End of section 16

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -1,4 +1,6 @@
 using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -8,8 +10,10 @@ namespace API.Controllers
     public class BasketController : BaseApiController
     {
         private readonly IBasketRepository _basketRepositiry;
-        public BasketController(IBasketRepository basketRepositiry)
+        private readonly IMapper _mapper;
+        public BasketController(IBasketRepository basketRepositiry, IMapper mapper)
         {
+            _mapper = mapper;
             _basketRepositiry = basketRepositiry;
         }
 
@@ -22,9 +26,10 @@ namespace API.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasketDto basket)
         {
-            var UpdateBasket = await _basketRepositiry.UpdateBasketAsync(basket);
+            var customerBasket = _mapper.Map<CustomerBasketDto, CustomerBasket>(basket);
+            var UpdateBasket = await _basketRepositiry.UpdateBasketAsync(customerBasket);
 
             return Ok(UpdateBasket);
         }
--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$",
+        ErrorMessage = "Password must have 1 Uppercase, 1 Lowercase, 1 Number, 1 non alphanumeric and at least 6 characters")]
         public string Password { get; set; }
     }
 }
